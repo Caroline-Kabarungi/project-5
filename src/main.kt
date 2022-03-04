ec7eb13bc40b4da606be816e4b84c0afdd8f2bf4
Caroline-Kabarungi <josephkabarungicaroline@gmail.com>
2022-03-04 17:09:31 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main(){
+var text= "Akirachix"
+
+    var campus =  text[0].toString()+text[2] + text[3]
+    println(campus)
+var c =statement("carotibz",45)
+    println(c)
+    var k =suzie()
+    println(k)
+    special("caroline")
+    special("Isabellah")
+
+
+}
+fun statement(name:String, years: Int):String{
+    var t=("hi, my name is $name and i am  $years old")
+    return (t)
+}
+fun suzie() :Int{
+    var suzie ="joseph"
+    var l =suzie.length
+    return l
+}
+fun special(name:String){
+    if(name=="caroline")
+        println("that is me")
+    else
+        println("that is not me")
+}
+
+
